b2b7fdbc8bc0636a27bf509a0ac8b0e531b546f0
Cognitive <alok.g@cognitiveclouds.com>
2016-10-24 17:13:32 +0530

    Added chat icon

--- app/src/main/java/app/football/com/footballapp/activity/MainActivity.java
+++ app/src/main/java/app/football/com/footballapp/activity/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -51,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
                 initInterface();
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if( item.getItemId() == R.id.action_chat ) {
+            startChat();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startChat() {
 
     }
 
@@ -104,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
         //Sort teams as per total score and GD
         Collections.sort(footballTeams, new Comparator<FootballTeam>() {
             @Override
-            public int compare(FootballTeam lhs, FootballTeam rhs) {
+            public int compare(FootballTeam rhs, FootballTeam lhs) {
                 int result = lhs.getTotalPoints() - rhs.getTotalPoints();
                 if( result != 0 )
                     return result;
